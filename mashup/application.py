93d52666eb209adb0969bbc710d04d93231d81e0
Dileep Nackathaya <36774780+dnackat@users.noreply.github.com>
2018-07-12 15:32:53 +0530

    Add files via upload

---
+++ mashup/application.py
@@ -0,0 +1,112 @@
+import os
+import re
+import string
+from flask import Flask, jsonify, render_template, request
+
+from cs50 import SQL
+from helpers import lookup
+
+# Configure application
+app = Flask(__name__)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///mashup.db")
+
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/")
+def index():
+    """Render map"""
+    return render_template("index.html")
+
+
+@app.route("/articles")
+def articles():
+    """Look up articles for geo"""
+
+    if not request.args.get("geo"):
+        raise RuntimeError("need location")
+
+    results = lookup(request.args.get("geo"))
+
+    if not results:
+        raise RuntimeError("invalid input")
+
+    return jsonify(results[:5])
+
+
+@app.route("/search")
+def search():
+    """Search for places that match query"""
+
+    if not request.args.get("q"):
+        raise RuntimeError("need location")
+
+    q = request.args.get("q") + "%"
+
+    # Check if zipcode or text
+    if (bool(re.search(r'\d', q))):
+        rows = db.execute("SELECT * FROM places WHERE postal_code LIKE :q LIMIT 10", q=q)
+    else:
+        q = q.replace("+", "").capitalize().split(",")
+        if len(q) == 1:
+            rows = db.execute("SELECT * FROM places WHERE place_name LIKE :q1 LIMIT 10", q1=q)
+        elif len(q) == 2:
+            rows = db.execute("SELECT * FROM places WHERE place_name LIKE :q1 OR admin_name1 LIKE :q2 LIMIT 10", q1=q[0], q2=q[1])
+
+    return jsonify(rows)
+
+
+@app.route("/update")
+def update():
+    """Find up to 10 places within view"""
+
+    # Ensure parameters are present
+    if not request.args.get("sw"):
+        raise RuntimeError("missing sw")
+    if not request.args.get("ne"):
+        raise RuntimeError("missing ne")
+
+    # Ensure parameters are in lat,lng format
+    if not re.search("^-?\d+(?:\.\d+)?,-?\d+(?:\.\d+)?$", request.args.get("sw")):
+        raise RuntimeError("invalid sw")
+    if not re.search("^-?\d+(?:\.\d+)?,-?\d+(?:\.\d+)?$", request.args.get("ne")):
+        raise RuntimeError("invalid ne")
+
+    # Explode southwest corner into two variables
+    sw_lat, sw_lng = map(float, request.args.get("sw").split(","))
+
+    # Explode northeast corner into two variables
+    ne_lat, ne_lng = map(float, request.args.get("ne").split(","))
+
+    # Find 10 cities within view, pseudorandomly chosen if more within view
+    if sw_lng <= ne_lng:
+
+        # Doesn't cross the antimeridian
+        rows = db.execute("""SELECT * FROM places
+                          WHERE :sw_lat <= latitude AND latitude <= :ne_lat AND (:sw_lng <= longitude AND longitude <= :ne_lng)
+                          GROUP BY country_code, place_name, admin_code1
+                          ORDER BY RANDOM()
+                          LIMIT 10""",
+                          sw_lat=sw_lat, ne_lat=ne_lat, sw_lng=sw_lng, ne_lng=ne_lng)
+
+    else:
+
+        # Crosses the antimeridian
+        rows = db.execute("""SELECT * FROM places
+                          WHERE :sw_lat <= latitude AND latitude <= :ne_lat AND (:sw_lng <= longitude OR longitude <= :ne_lng)
+                          GROUP BY country_code, place_name, admin_code1
+                          ORDER BY RANDOM()
+                          LIMIT 10""",
+                          sw_lat=sw_lat, ne_lat=ne_lat, sw_lng=sw_lng, ne_lng=ne_lng)
+
+    # Output places as JSON
+    return jsonify(rows)
